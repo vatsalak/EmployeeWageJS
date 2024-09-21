fe7514ebc013f5fd4e12dc4484ef023f5a31135d
Vatsala Kaashyap <vatsalakaashyap@gmail.com>
2024-09-21 19:56:53 +0530

    Done checking attendance

--- EmployeeWageComputation/Employee.js
+++ EmployeeWageComputation/Employee.js
@@ -1,1 +1,16 @@
-console.log("Welcome to the Employee Wage computation");
+// Function to check employee attendance
+function checkAttendance() {
+    // Generate a random number between 0 and 1
+    let attendance = Math.floor(Math.random() * 2); // 0 or 1
+  
+    // Check if employee is present or absent
+    if (attendance === 1) {
+      console.log("Employee is Present");
+    } else {
+      console.log("Employee is Absent");
+    }
+  }
+  
+  // Call the function to check attendance
+  checkAttendance();
+  
